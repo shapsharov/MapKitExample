c914c0586db9bc9f55b00d8a5dd5b0764fda9ae3
shapsharov <shapsharov@gmail.com>
2021-10-31 20:25:07 +0300

    Initial Commit

---
+++ MapKitExample/MapKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitExampleApp.swift
+//  MapKitExample
+//
+//  Created by Bogdan Shapsharov on 31.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MapKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
